d6c7c075676015ece59ae466f90b6186f8b57369
Dronswa <dronswa@gmail.com>
2026-04-22 22:06:05 +0700

    Fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,14 +2,16 @@ import _ from 'lodash'
 
 const stringify = (value, depth) => {
   if (!_.isObject(value)) {
-    return String(value)
+    return value
   }
 
-  const indent = '    '.repeat(depth + 1)
-  const entries = Object.entries(value)
-  const lines = entries.map(([key, val]) => `${indent}${key}: ${stringify(val, depth + 1)}`)
+  const indent = '    '.repeat(depth)
+  const lines = Object.entries(value).map(([key, val]) => {
+    const formattedValue = _.isObject(val) ? stringify(val, depth + 1) : val
+    return `${indent}    ${key}: ${formattedValue}`
+  })
 
-  return `{\n${lines.join('\n')}\n${indent.slice(0, -4)}}`
+  return `{\n${lines.join('\n')}\n${indent}}`
 }
 
 const formatStylish = (diff, depth = 0) => {
@@ -17,13 +19,13 @@ const formatStylish = (diff, depth = 0) => {
   const result = diff.map((item) => {
     switch (item.type) {
       case 'added':
-        return `${indent}  + ${item.key}: ${stringify(item.value, depth)}`
+        return `${indent}  + ${item.key}: ${stringify(item.value, depth + 1)}`
       case 'removed':
-        return `${indent}  - ${item.key}: ${stringify(item.value, depth)}`
+        return `${indent}  - ${item.key}: ${stringify(item.value, depth + 1)}`
       case 'unchanged':
-        return `${indent}    ${item.key}: ${stringify(item.value, depth)}`
+        return `${indent}    ${item.key}: ${stringify(item.value, depth + 1)}`
       case 'changed':
-        return `${indent}  - ${item.key}: ${stringify(item.oldValue, depth)}\n${indent}  + ${item.key}: ${stringify(item.newValue, depth)}`
+        return `${indent}  - ${item.key}: ${stringify(item.oldValue, depth + 1)}\n${indent}  + ${item.key}: ${stringify(item.newValue, depth + 1)}`
       case 'nested':
         return `${indent}    ${item.key}: ${formatStylish(item.children, depth + 1)}`
       default:
